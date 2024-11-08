9168aae6be4eb0a31855e85f5c36e38b3ee5009d
David Scalais <david.scalais@student.uantwerpen.be>
2024-11-08 16:41:51 +0100

    reply with pong message

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
 
-    server_socket.send('+PONG\r\n')
+    server_socket.send(b'+PONG\r\n')
 
 if __name__ == "__main__":
     main()
