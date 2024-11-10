e732e873c0d8b55b7d05b5afcccd4120f2951944
David Scalais <david.scalais@student.uantwerpen.be>
2024-11-10 12:50:22 +0100

    reply with pong message

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import socket  # noqa: F401
 
-
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -8,9 +7,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    client, addr = server_socket.accept() # wait for client
 
-    server_socket.send(b'+PONG\r\n')
+
+    client.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
